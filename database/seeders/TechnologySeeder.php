af5a5b616532c91555b23d358f9e209b877c31a0
glbdmn <gabrieledamiani@hotmail.it>
2023-06-23 14:32:14 +0200

    stampati i dati tabella sulla show

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,20 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = [
+            'pc',
+            'iphone',
+            'mac',
+            'android',
+            'tablet'
+        ];
+
+
+        foreach($technologies as $elem){
+            $new_technology = new Technology();
+            $new_technology->name = $elem;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+        };
     }
 }
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,15 +1,32 @@
+@vite(['resources/js/app.js'])
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+ <div class="container">
     <h1>{{ $project->title }}</h1>
     <p>{{ $project->content }}</p>
     <p>{{ $project->slug }}</p>
     <img src="{{ asset('storage/' . $project->cover_image) }}" alt="">
 
-    <div>
-        {{ $project->type->name }}
-    </div>
 
-</div>
+    <h2>type</h2>
+    @if($project->type)
+    <div>nome:{{ $project->type->name }} </div>
+    <div>slug:{{ $project->slug }}</div>
+    @endif
+
+    <h2>technologies</h2>
+    {{-- @if($project->technologies)
+    @foreach($project->technologies as $elem)
+    <div>technology name: {{ $elem->name }}</div> 
+    @endforeach
+    @endif
+    --}}
+    @php
+        // non riesco ad ottenere i record
+        dd($project->technologies)
+    @endphp
+
+
+ </div>
 @endsection
